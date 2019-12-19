f5837c3f8dc01aed57b1ba9acd9c367db898820e
OnismoMudavanhu <onismomudavanhu@gmail.com>
2019-12-19 09:48:32 +0200

    services items and top bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import {Link, NavLink} from 'react-router-dom';
+
 
 
 
--- src/pages/Industries.js
+++ src/pages/Industries.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import Background from './industries.jpg';
+
+var sectionStyle = {
+    width: "100%",
+    height: "550px",
+    backgroundImage: "url(" + Background + ")"
+  };
 
 export default class Industries extends Component{
     render(){
         return(
-            <div className="text-center text-5xl h-56 mt-56">
+            <div className="">
+                <section style={ sectionStyle }>
+                    <div className="pt-10 lg:pt-32 mx-4 md:mx-20">
+                       <h1 className="text-2xl md:text-5xl text-gray-200 font-semibold text-center">Industries</h1>
+                       </div>
+                </section>
                 Industries
             </div>
         );
